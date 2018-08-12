d6ff6681faf88e4de85d8ab26482c1afe8d6f41a
Chris gutierrez <chris201234@gmail.com>
2018-08-12 15:35:44 -0500

    new file:   ranger/plugins/devicons_linemode.py 	modified:   restoredotFiles.sh

--- restoredotFiles.sh
+++ restoredotFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a arr=("i3/config" "compton.conf" "nvim/init.vim" "ranger/plugins/__init__.py" "ranger/devicons.py" "ranger/rifle.conf" "ranger/rc.conf" "youtube-dl/" )
+declare -a arr=("i3/config" "compton.conf" "nvim/init.vim" "ranger/plugins/devicons_linemode.py" "ranger/plugins/__init__.py" "ranger/devicons.py" "ranger/rifle.conf" "ranger/rc.conf" "youtube-dl/" )
 declare -a arr2=(".Xresources" ".xinitrc" ".tmux.conf" ".bashrc" ".zshrc")
 
 config=".config/"
@@ -7,12 +7,12 @@ destination="~/Repositories/archdotfiles"
 for i in "${arr[@]}"
 do
         
-    rsync  -arP  ~/Repositories/archdotfiles/${i%/*} ~/$config
+    rsync  -arP  ${i%/*} ~/$config
 done
 
 
 for i in "${arr2[@]}"
 do
-    rsync  -aP  ~/Repositories/archdotfiles/$i ~/
+    rsync  -aP  $i ~/
 done
 
